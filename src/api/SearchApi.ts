33ffea9f6b3e68e343daa0c64ac83b22bc7cd5f6
강세민 <ksm9802@gmail.com>
2023-05-04 09:01:17 +0900

    chore: netlify.toml에 :splat 추가 및 proxy 앞에 '/' 추가

--- src/api/SearchApi.ts
+++ src/api/SearchApi.ts
@@ -1,7 +1,7 @@
 import customAxios from "../lib/customAxios";
 
 const getRecommendation = async (name: string) => {
-  const PROXY = window.location.hostname === "localhost" ? "" : "proxy";
+  const PROXY = window.location.hostname === "localhost" ? "" : "/proxy";
   const res = await customAxios.get(`${PROXY}/api/v1/search-conditions/?name=${name}`);
   return res.data;
 };
